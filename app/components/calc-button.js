63633f5d8c0f8a42b466e67ec8f34c0a06e16b6a
Benedikt Dichgans <dichgans@eundd.com>
2015-05-13 19:40:50 +0200

    Keyboard shortcuts 0-9 + ,

--- app/components/calc-button.js
+++ app/components/calc-button.js
@@ -34,5 +34,50 @@ export default Ember.Component.extend({
   click: function() {
     var value = this.get("value");
     this.sendAction("action", value);
+  },
+
+  keyDown: function(event) {
+    var key = event.keyCode;
+    switch(key) {
+      case 48:
+      case 96:  key = 0;
+      break;
+      case 49:
+      case 97: key = 1;
+      break;
+      case 50:
+      case 98: key = 2;
+      break;
+      case 51:
+      case 99: key = 3;
+      break;
+      case 52:
+      case 100: key = 4;
+      break;
+      case 53:
+      case 101: key = 5;
+      break;
+      case 54:
+      case 102: key = 6;
+      break;
+      case 55:
+      case 103: key = 7;
+      break;
+      case 56:
+      case 104: key = 8;
+      break;
+      case 57:
+      case 105: key = 9;
+      break;
+      case 110:
+      case 188:
+      case 190: key = ".";
+      break;
+      default: key = ""
+    }
+    var value = key;
+    this.sendAction("action", value);
+    
+    //console.log("value");
   }
 });
